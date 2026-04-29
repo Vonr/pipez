b6c5af890ab65a7fa0e4777c12b5d8203cf1d1ab
Qther <qther@tuta.io>
2026-04-29 21:58:19 +0800

    clean up canInsertProto

--- src/main/java/de/maxhenkel/pipez/blocks/tileentity/types/PipeType.java
+++ src/main/java/de/maxhenkel/pipez/blocks/tileentity/types/PipeType.java
@@ -265,15 +265,11 @@ public abstract class PipeType<T, D extends AbstractPipeTypeData<T>> {
                 continue;
             }
 
-            var isNormal = !filter.isInvert();
-
             if (matcher.test(context, filter, stack)) {
-                return isNormal;
+                return !filter.isInvert();
             }
 
-            if (isNormal) {
-                noNormalFilters = false;
-            }
+            noNormalFilters &= filter.isInvert();
         }
 
         return noNormalFilters;
